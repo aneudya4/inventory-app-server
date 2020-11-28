ef26d8507b6d1f23853f638f3d67ddc25aa57dc8
Aneudy Adames <a4aneudy@gmail.com>
2020-11-28 00:45:58 -0500

    testing deployment

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const knex = require('knex');
-const app = require('./src/app');
+const app = require('./app');
 
-const { PORT, DATABASE_URL } = require('./src/config');
+const { PORT, DATABASE_URL } = require('./config');
 
 const db = knex({
   client: 'pg',
